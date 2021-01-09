2a5fb7a3ad631d5d5cc47c6071c46beb959f80ea
Worawuth Puntusithsaree <popcorns1udio@gmail.com>
2021-01-09 14:26:27 +0700

    112233

--- class/matting.js
+++ class/matting.js
@@ -5,9 +5,9 @@ var now = new Date();
 module.exports = function(app,con){
 
     app.post('/add/sowmating',(req,res)=>{
-        var sowSemenID = req.query.sowSemenID;
-        var sowID = req.query.sowID;
-        var userID = req.query.userID;
+        var sowSemenID = req.body.sowSemenID;
+        var sowID = req.body.sowID;
+        var userID = req.body.userID;
       
         var sql = "INSERT INTO sowmating(sowSemenID,sowID,userID,created_at,updated_at) Values('"+sowSemenID+"','"+sowID+"','"+userID+"','"+dateFormat(now,'yyyy-mm-dd HH:MM:ss')+"','"+dateFormat(now,'yyyy-mm-dd HH:MM:ss')+"')"
         con.query(sql,function(err,result,filed){
